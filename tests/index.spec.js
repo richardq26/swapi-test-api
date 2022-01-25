14c6993e8cd370ebd30ee6d064e44f8b267a99ff
richardq26 <richard.sw268@gmail.com>
2022-01-25 14:36:47 -0500

    jest en nuevos componentes

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,8 +1,11 @@
 const starships = require("../components/starships/services");
 const vehicles = require("../components/vehicles/services");
 const species = require("../components/species/services");
-const helpers= require("../helpers");
+const serieCharacter = require("../components/serieCharacter/services");
+const favorites = require("../components/favorites/services");
+const helpers = require("../helpers");
 
+// Pruebas unitarias de gets para probar conexión a base de datos y obtención de datos
 describe("GET /starships", () => {
   test("Paginate", () => {
     const response = starships.paginate;
@@ -35,6 +38,20 @@ describe("GET /vehicles", () => {
 
   test("Obtener uno", () => {
     const response = vehicles.getOne;
+    expect(response.toBeDefined);
+  });
+});
+
+describe("GET /serieCharacter", () => {
+  test("Paginate", () => {
+    const response = serieCharacter.paginate;
+    expect(response.toBeDefined);
+  });
+});
+
+describe("GET /favorites", () => {
+  test("Paginate", () => {
+    const response = favorites.getFavorites;
     expect(response.toBeDefined);
   });
 });
